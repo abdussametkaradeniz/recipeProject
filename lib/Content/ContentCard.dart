fc66a3c60adee251c6892ad56f021e450a473451
abdussamet karadeniz <abdussametgosukaradeniz@gmail.com>
2023-02-17 10:53:19 +0300

    proje dosya yapısı hazırlandı

---
+++ lib/Content/ContentCard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContentCard extends StatefulWidget {
+  const ContentCard({super.key});
+
+  @override
+  State<ContentCard> createState() => _ContentCardState();
+}
+
+class _ContentCardState extends State<ContentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/TopBar/TopBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TopBar extends StatefulWidget {
+  const TopBar({super.key});
+
+  @override
+  State<TopBar> createState() => _TopBarState();
+}
+
+class _TopBarState extends State<TopBar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
